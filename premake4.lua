83d2a8f86c7a71f0e86891c283c8af05c9596d84
Ash Berlin <ash_github@firemirror.com>
2009-09-29 13:35:19 +0100

    A simple (non portable) premake file

---
+++ premake4.lua
@@ -0,0 +1,24 @@
+solution "Zest"
+
+  targetdir "build"
+
+  configurations { "Release" }
+  
+  project "libzest"
+    targetname "zest"
+    targetextension ".dylib"
+
+    kind "SharedLib"
+    language "C++"
+    files { "src/*.cpp", "src/*.hpp" }
+
+    includedirs { 
+      "/usr/local/include/boost-1_37",
+      "/Users/ash/code/js/mozjs_debug/include"
+    }
+
+    libdirs {
+      "/Users/ash/code/js/mozjs_debug/lib"
+    }
+
+    links { "flusspferd" }

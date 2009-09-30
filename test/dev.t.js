0e08b06cf40f4e15a3c1931f254aabd7a287e5e7
Ash Berlin <ash_github@firemirror.com>
2009-09-30 19:47:36 +0100

    More simple testing.

--- test/dev.t.js
+++ test/dev.t.js
@@ -5,9 +5,14 @@ var z = require('zest');
 var server = z.Zest({
   handler: function(req) {
     print(uneval(req));
+
+    req.jsgi.errors.print("*hello");
     return {
       status: 200,
-      body: [1]
+      body: ["I'm content from " + module.uri],
+      headers: {
+        'content-type': 'text/plain'
+      }
     }
   }
 });

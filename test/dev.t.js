f1edcb5e1ccbac69123112cbfbb771bb0acc1599
Ash Berlin <ash_github@firemirror.com>
2009-10-25 16:17:17 +0000

    Dump request env in test

--- test/dev.t.js
+++ test/dev.t.js
@@ -5,15 +5,17 @@ var z = require('zest');
 var server = z.Zest({
   handler: function(req) {
 
-    var blob = req.body.read(4);
-    req.jsgi.errors.print(blob.decodeToString());
+    print(uneval(req));
+    /*var blob = req.body.read(4);
+    req.jsgi.errors.print(blob.decodeToString());*/
 
     req.jsgi.errors.print("*hello");
     return {
       status: 200,
       body: ["I'm content from " + module.uri],
       headers: {
-        'content-type': 'text/plain'
+        //'x-sendfile': '/Users/ash/.bashrc',
+        //'content-type': 'text/plain'
       }
     }
   }
